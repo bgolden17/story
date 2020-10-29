5417c3ad2fae872d8a65083bcdccd396098449eb
Sean <Arlesken1@hotmail.com>
2020-10-29 11:20:01 -0400

    changed monkeys to students

--- src/StoryPractice.java
+++ src/StoryPractice.java
@@ -3,11 +3,11 @@ public class StoryPractice {
 	
 	public static void main(String[] args) {
 	
-		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-		System.out.println("Kai-Yun and his merry monkey band could not take anything seriously, they were always monkeying around.");
+		System.out.println("Once upon a time in the mystical land of Detroit there dwelt an immortal named Kai-Yun and his trusty band of ninja students.");
+		System.out.println("Kai-Yun and his merry student band could not take anything seriously, they were always monkeying around.");
 		System.out.println("Then the ninja Gorilla's show up");
-		System.out.println("The gorillas charged the monkeys, shrieking as they approached.");
-		System.out.println("Kai-Yun's monkey allies fought hard but were no match for the stronger gorillas.");
+		System.out.println("The gorillas charged the students, shrieking as they approached.");
+		System.out.println("Kai-Yun's student allies fought hard but were no match for the stronger gorillas.");
 		System.out.println("Then they all worked out there differences and went to Taco Bell");
 	}
 }

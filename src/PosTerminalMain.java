657d4e8c9c412681c7ab44c33c8bc7a0fd97351a
Sean <Arlesken1@hotmail.com>
2020-10-29 13:43:29 -0400

    first project push w notes

---
+++ src/PosTerminalMain.java
@@ -0,0 +1,38 @@
+
+public class PosTerminalMain {
+
+	public static void main(String[] args) {
+		//choose store type -- Coffee Shop 
+		
+		//create product class w name, category, description. 
+		
+		//store products in a text file, 12 items minimum.
+		
+		//Project includes at least 3 classes; classes interact meaningfully. Methods are short and modular
+		// 1 main class, 2 product class, <-- 3 child class to product class
+		
+		// Main:
+		//present menu, let user choose item (by number or letter) and quantity. display line total (item price * quantity)
+		//Either through the menu or a separate question, allow them to re-display the menu and to complete the purchase
+		//Give the subtotal, sales tax, and grand total
+		
+		
+		//Main paymentOption method:
+		//Ask for payment type—cash, credit, or check
+		//For cash, ask for amount tendered and provide change
+		//For check, get the check number.
+		//For credit, get the credit card number, expiration, and CVV.
+		
+		//Main totalCalc Method;
+		//At the end, display a receipt with all items ordered, subtotal, grand total, and appropriate payment info.
+		
+		//Main menu Method;
+		//Return to the original menu for a new order. (Hint: you’ll want an array or ArrayList to keep track of what’s been ordered!)
+		
+		
+		
+
+
+	}
+
+}

36b3e3b1e7e2332c4a1b13eb4bd319fbc9e6f774
Sean <Arlesken1@hotmail.com>
2020-10-29 09:56:17 -0400

    second print line

--- src/StoryPractice.java
+++ src/StoryPractice.java
@@ -4,6 +4,7 @@ public class StoryPractice {
 	public static void main(String[] args) {
 	
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Kai-Yun and his merry monkey band could not take anything seriously, they were always monkeying around.");
 		
 	}
 }
